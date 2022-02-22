bf4fbae03568d723519d4aee89b05119d997b331
Victor Engmark <vengmark@linz.govt.nz>
2022-02-23 09:26:00 +1300

    feat: Parameterise extra packages
    Rather than install all packages needed to build all other packages,
    each package should list its own additional dependencies.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -o errexit -o nounset
 
 release="$1"
+packages="$2"
 
 tag="linz-deb-builder"
 project_name="$(basename "$GITHUB_REPOSITORY")"
@@ -10,5 +11,5 @@ host_repo_dir="${RUNNER_WORKSPACE}/${project_name}"
 
 cd /docker-action
 
-docker build --build-arg=DISTRIBUTION="$release" --tag="$tag" .
+docker build --build-arg=DISTRIBUTION="$release" --build-arg=EXTRA_PACKAGES="$packages" --tag="$tag" .
 docker run --env=PACKAGECLOUD_TOKEN --env=PACKAGECLOUD_REPOSITORY --env=PUSH_TO_GIT_REMOTE --env=DRY_RUN --volume="${host_repo_dir}:/pkg" "$tag"
